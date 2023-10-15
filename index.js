4334afcfbbd17cbb36550b790f0f98de17f59325
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2023-10-15 04:33:07 -0600

    style: input bg remove on error

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const form = document.querySelector(".form");
 const emailInput = document.getElementById("email");
 const errorMessage = document.querySelector(".error-message");
 
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     validate();
 })
 
-document.addEventListener('click', ()=>{
+document.addEventListener('click', () => {
     emailInput.style.border = "1px solid var(--Pale-Blue)";
     emailInput.style.outline = "none";
     emailInput.style.backgroundColor = "transparent";
@@ -15,19 +15,18 @@ document.addEventListener('click', ()=>{
     errorMessage.style.color = "var(--Light-Red)";
 })
 
-function validate(){
-    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+function validate() {
+    let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     let isValid = expReg.test(emailInput.value);
 
-    if(!isValid){        
+    if (!isValid) {
         errorMessage.style.color = "var(--Light-Red)";
-        errorMessage.textContent = "please provide a valid email address";
+        errorMessage.textContent = "Please provide a valid email address";
         emailInput.style.borderColor = "var(--Light-Red)";
-        emailInput.style.backgroundColor = "var(--Pale-Red)";
     }
-    else{
+    else {
         errorMessage.style.color = "var(--Pale-Green)";
-        errorMessage.textContent = "email accepted";   
+        errorMessage.textContent = "email accepted";
         emailInput.style.outline = "1px solid var(--Pale-Green)";
         emailInput.style.backgroundColor = "var(--Light-Green)";
         emailInput.value = "";
